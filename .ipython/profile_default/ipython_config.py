1e9bdb8e91f0c40d3821eaf8a4712501a4219a95
zcesur <17045339+zcesur@users.noreply.github.com>
2020-02-02 05:29:53 +0300

    Delete unused things

--- .ipython/profile_default/ipython_config.py
+++
@@ -1,5 +0,0 @@
-c = get_config()
-c.TerminalInteractiveShell.editing_mode = 'vi'
-c.TerminalInteractiveShell.confirm_exit = False
-c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['%autoreload 2']
