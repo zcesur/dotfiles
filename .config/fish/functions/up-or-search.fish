201d6a0e36f784de062b441c4b9586365f7a084e
zcesur <17045339+zcesur@users.noreply.github.com>
2020-02-02 16:34:50 +0300

    Replace up-or-search function with one that uses history-prefix-search-backward

--- .config/fish/functions/up-or-search.fish
+++ .config/fish/functions/up-or-search.fish
@@ -1,1 +1,31 @@
-../plugins/fish-history-merge/functions/up-or-search.fish
+# References:
+# https://github.com/fish-shell/fish-shell/issues/825#issuecomment-440286038
+# https://github.com/2m/fish-history-merge/blob/master/functions/up-or-search.fish
+
+function up-or-search -d "Depending on cursor position and current mode, either prefix search backward or move up one line"
+    # If we are already in search mode, continue
+    if commandline --search-mode
+        commandline -f history-prefix-search-backward
+        return
+    end
+
+    # If we are navigating the pager, then up always navigates
+    if commandline --paging-mode
+        commandline -f up-line
+        return
+    end
+
+    # We are not already in search mode.
+    # If we are on the top line, start search mode,
+    # otherwise move up
+    set lineno (commandline -L)
+
+    switch $lineno
+        case 1
+            commandline -f history-prefix-search-backward
+            history merge
+
+        case '*'
+            commandline -f up-line
+    end
+end
